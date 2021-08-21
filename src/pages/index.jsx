3fca9abd598be5670f1ce43eeb533eaf096807c5
Lucas <lucas-mateus.dc@hotmail.com>
2021-08-21 10:58:19 -0300

    fix meta-name

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -17,7 +17,7 @@ export default function Home() {
         <meta name="description" content="Mais de 18 anos transformando sonhos em realidade através da construção civíl. Venha fazer um orçamento conosco!" />
         <meta name="robots" content="index, follow" /> 
         <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         <link rel="icon" href="/favicon.ico" />
 
       </Head>
